5f06f0ce595e47c1f826ab10ebc41b9b7c95e0ba
JayavelAnandh <jvanandh984@gmail.com>
2023-02-27 10:36:00 +0530

    first commit

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ import cors from "cors";
 import { productsRouter } from "./routes/products.js"
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("Hello i'm working fine")
+ })
+
 app.use(express.static("express"));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 
 app.use("/data",productsRouter)
@@ -15,7 +19,5 @@ app.use("/data",productsRouter)
 //     res.status(404).send({Response:"Data not found"})
     
 // })
-app.get("/", (req, res)=>{
-    res.send("Hello i'm working fine")
- })
+
 app.listen(9000, () => console.log(`server started localhost: 9000`));
